c94090900a05c490b5412b55b5b6fe73a04e314f
akbarasadov <akbarasadov4170@gmail.com>
2025-12-29 19:06:51 +0500

    new upload

--- public/script.js
+++ public/script.js
@@ -1,15 +1,21 @@
 const socket = io();
-const messages = document.getElementById("messages");
-const input = document.getElementById("input");
+let selfName = "Siz"; // keyin username qo‘shsak oson o‘zgaradi
 
 function send() {
+  const input = document.getElementById("input");
   if (input.value.trim() === "") return;
-  socket.emit("chat message", input.value);
+  socket.emit("chat message", { msg: input.value, name: selfName });
   input.value = "";
 }
 
-socket.on("chat message", (msg) => {
+socket.on("chat message", (data) => {
   const div = document.createElement("div");
-  div.textContent = msg;
-  messages.appendChild(div);
+  if (data.name === selfName) {
+    div.className = "self";
+  } else {
+    div.className = "other";
+  }
+  div.innerText = `${data.name}: ${data.msg}`;
+  document.getElementById("messages").appendChild(div);
+  document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
 });
