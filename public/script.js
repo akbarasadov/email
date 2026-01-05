67776f873e2c90bcd7d82982bbd64c56e1d86b92
akbarasadov <akbarasadov4170@gmail.com>
2026-01-05 12:14:33 +0500

    public_js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,5 @@
 const name = localStorage.getItem("name");
-
-if (!name) {
-  alert("Iltimos, avval ro‘yxatdan o‘ting");
-  window.location.href = "/";
-}
+if (!name) window.location.href = "/";
 
 const socket = io({
   auth: { name }
@@ -15,7 +11,12 @@ const sendBtn = document.getElementById("sendBtn");
 
 sendBtn.addEventListener("click", () => {
   if (!input.value.trim()) return;
-  socket.emit("chat message", { name, msg: input.value });
+
+  socket.emit("chat message", {
+    name,
+    msg: input.value
+  });
+
   input.value = "";
 });
 
@@ -23,11 +24,11 @@ socket.on("chat message", data => {
   const div = document.createElement("div");
 
   if (data.name === name) {
-    div.innerHTML = `<b>${name}</b>: ${data.msg}`;
     div.classList.add("right_massage");
+    div.innerHTML = `<b>Me</b>: ${data.msg}`;
   } else {
-    div.innerHTML = `<b>${data.name}</b>: ${data.msg}`;
     div.classList.add("left_massage");
+    div.innerHTML = `<b>${data.name}</b>: ${data.msg}`;
   }
 
   messages.appendChild(div);
