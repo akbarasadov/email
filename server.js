5f884e9fe466b1cf88b0ba0018b361a0912d20d1
akbarasadov <akbarasadov4170@gmail.com>
2026-01-06 18:58:13 +0500

    15

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
 const express = require("express");
 const http = require("http");
+const fs = require("fs");
 const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/chat", (req, res) => {
-  res.sendFile(path.join(__dirname, "chat.html"));
-});
-
-// ===== SOCKET =====
 const io = new Server(server);
 
-let messages = []; // 🧠 XABARLAR SAQLANADI
+app.use(express.static(__dirname));
+app.use(express.json());
+
+const MSG_FILE = "./messages.json";
+
+function readMessages() {
+  if (!fs.existsSync(MSG_FILE)) return [];
+  return JSON.parse(fs.readFileSync(MSG_FILE, "utf8"));
+}
+
+function saveMessages(msgs) {
+  fs.writeFileSync(MSG_FILE, JSON.stringify(msgs, null, 2));
+}
 
 io.on("connection", socket => {
-  console.log("Client connected");
+  socket.emit("old messages", readMessages());
 
-  // eski xabarlar
-  socket.emit("old messages", messages);
-
-  // yangi xabar
   socket.on("chat message", msg => {
-    messages.push(msg);
+    const msgs = readMessages();
+    msgs.push(msg);
+    saveMessages(msgs);
     io.emit("chat message", msg);
   });
 
-  // xabar o‘chirish
+  socket.on("read message", id => {
+    io.emit("read message", id);
+  });
+
   socket.on("delete message", id => {
-    messages = messages.filter(m => m.id !== id);
+    let msgs = readMessages().filter(m => m.id !== id);
+    saveMessages(msgs);
     io.emit("delete message", id);
   });
 });
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log("Server running on", PORT);
+server.listen(8080, () => {
+  console.log("Server running on 8080");
 });
